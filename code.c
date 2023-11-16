e3a6e9f5e24456b76c7e180336fade7f9b98a793
nishantvg <150948213+nishantvg@users.noreply.github.com>
2023-11-16 11:46:34 +0530

    code.c

--- code.c
+++ code.c
@@ -6,70 +6,85 @@
 #define MAX_POSTS 100
 
 // User data structure
-typedef struct {
+typedef struct
+{
     int id;
     char username[50];
     char password[50];
 } User;
 
 // Graph data structure (Adjacency List)
-typedef struct Node {
+typedef struct Node
+{
     int userId;
-    struct Node* next;
+    struct Node *next;
 } Node;
 
 // Graph data structure (For Networking)
-typedef struct {
+typedef struct
+{
     User user;
-    Node* friends;
+    Node *friends;
 } Graph;
 
 //  Data structure for Post
-typedef struct {
+typedef struct
+{
     int id;
     int authorId;
     char content[500];
 } Post;
 
-Graph socialNetwork[MAX_USERS];
+Graph social_network[MAX_USERS];
 Post posts[MAX_POSTS];
-int userCount = 0;
-int postCount = 0;
-int currentUserIndex = -1;
+int user_count = 0;
+int post_count = 0;
+int user_index = -1;
 
 // Function to find the user index by username
-int findUserIndex(const char* username) {
-    for (int i = 0; i < userCount; i++) {
-        if (strcmp(socialNetwork[i].user.username, username) == 0) {
+int find_index(const char *username)
+{
+    for (int i = 0; i < user_count; i++)
+    {
+        if (strcmp(social_network[i].user.username, username) == 0)
+        {
             return i;
         }
     }
-    return -1; 
+    return -1;
 }
 
 // Function to register a new user
-void registerUser() {
-    if (userCount < MAX_USERS) {
-        User newUser;
+void register_user()
+{
+    if (user_count < MAX_USERS)
+    {
+        User new_user;
         printf("Enter username: ");
-        scanf("%s", newUser.username);
+        scanf("%s", new_user.username);
         printf("Enter password: ");
-        scanf("%s", newUser.password);
-        newUser.id = userCount;
-        socialNetwork[userCount].user = newUser;
-        socialNetwork[userCount].friends = NULL;
-        userCount++;
+        scanf("%s", new_user.password);
+        new_user.id = user_count;
+        social_network[user_count].user = new_user;
+        social_network[user_count].friends = NULL;
+        user_count++;
         printf("Registration successful!\n");
-    } else {
+    }
+    else
+    {
         printf("User limit reached!\n");
     }
 }
 
 // Function to log in a user
-void login() {
-    if (currentUserIndex != -1) {
-        printf("You are already logged in as %s.\n", socialNetwork[currentUserIndex].user.username);
-    } else {
+void login()
+{
+    if (user_index != -1)
+    {
+        printf("You are already logged in as %s.\n", social_network[user_index].user.username);
+    }
+    else
+    {
         char username[50];
         char password[50];
         printf("Enter username: ");
@@ -77,117 +92,141 @@ void login() {
         printf("Enter password: ");
         scanf("%s", password);
 
-        int userIndex = findUserIndex(username);
+        int userIndex = find_index(username);
 
-        //To store the user to the social network
-        if (userIndex != -1 && strcmp(socialNetwork[userIndex].user.password, password) == 0) {
-            currentUserIndex = userIndex;
+        // To store the user to the social network
+        if (userIndex != -1 && strcmp(social_network[userIndex].user.password, password) == 0)
+        {
+            user_index = userIndex;
             printf("Login successful!\n");
-        } else {
+        }
+        else
+        {
             printf("Invalid username or password!\n");
         }
     }
 }
 
 // Function to add a friend
-void addFriend() {
-    if (currentUserIndex == -1) {
+void add_friend()
+{
+    if (user_index == -1)
+    {
         printf("Please log in first!\n");
         return;
     }
 
-    char friendUsername[50];
+    char friend_name[50];
     printf("Enter the username of the friend you want to add: ");
-    scanf("%s", friendUsername);
+    scanf("%s", friend_name);
 
-    int friendIndex = findUserIndex(friendUsername);
+    int friendIndex = find_index(friend_name);
 
-    if (friendIndex == -1) {
+    if (friendIndex == -1)
+    {
         printf("User not found.\n");
         return;
     }
 
     // Check if they are already friends
-    Node* currentFriend = socialNetwork[currentUserIndex].friends;
-    while (currentFriend != NULL) {
-        if (currentFriend->userId == friendIndex) {
+    Node *curr_friend = social_network[user_index].friends;
+    while (curr_friend != NULL)
+    {
+        if (curr_friend->userId == friendIndex)
+        {
             printf("You are already friends with this user.\n");
             return;
         }
-        currentFriend = currentFriend->next;
+        curr_friend = curr_friend->next;
     }
 
     // Add friend to the adjacency list
-    Node* newFriendNode = (Node*)malloc(sizeof(Node));
-    newFriendNode->userId = friendIndex;
-    newFriendNode->next = socialNetwork[currentUserIndex].friends;
-    socialNetwork[currentUserIndex].friends = newFriendNode;
+    Node *new_friend = (Node *)malloc(sizeof(Node));
+    new_friend->userId = friendIndex;
+    new_friend->next = social_network[user_index].friends;
+    social_network[user_index].friends = new_friend;
 
     // Add current user to the friend's adjacency list
-    Node* currentUserNode = (Node*)malloc(sizeof(Node));
-    currentUserNode->userId = currentUserIndex;
-    currentUserNode->next = socialNetwork[friendIndex].friends;
-    socialNetwork[friendIndex].friends = currentUserNode;
+    Node *curr_node = (Node *)malloc(sizeof(Node));
+    curr_node->userId = user_index;
+    curr_node->next = social_network[friendIndex].friends;
+    social_network[friendIndex].friends = curr_node;
 
-    printf("You are now friends with %s.\n", friendUsername);
+    printf("You are now friends with %s.\n", friend_name);
 }
 
 // Function to view a user's list of friends
-void viewFriends() {
-    if (currentUserIndex == -1) {
+void view_friends()
+{
+    if (user_index == -1)
+    {
         printf("Please log in first!\n");
         return;
     }
 
-    printf("Friends of %s:\n", socialNetwork[currentUserIndex].user.username);
-    Node* currentFriend = socialNetwork[currentUserIndex].friends;
-    while (currentFriend != NULL) {
-        printf("%s\n", socialNetwork[currentFriend->userId].user.username);
-        currentFriend = currentFriend->next;
+    printf("Friends of %s:\n", social_network[user_index].user.username);
+    Node *curr_friend = social_network[user_index].friends;
+    while (curr_friend != NULL)
+    {
+        printf("%s\n", social_network[curr_friend->userId].user.username);
+        curr_friend = curr_friend->next;
     }
 }
 
 // Function to post updates
-void postUpdate() {
-    if (currentUserIndex == -1) {
+void add_post()
+{
+    if (user_index == -1)
+    {
         printf("Please log in first!\n");
         return;
     }
 
-    if (postCount < MAX_POSTS) {
-        Post newPost;
-        newPost.id = postCount;
-        newPost.authorId = currentUserIndex;
+    if (post_count < MAX_POSTS)
+    {
+        Post new_post;
+        new_post.id = post_count;
+        new_post.authorId = user_index;
         printf("Enter your post (max 500 characters): ");
-        scanf(" %[^\n]", newPost.content);
-        posts[postCount++] = newPost;
+        scanf(" %[^\n]", new_post.content);
+        posts[post_count++] = new_post;
         printf("Post added!\n");
-    } else {
+    }
+    else
+    {
         printf("Post limit reached!\n");
     }
 }
 
 // Function to view the post
-void viewPost() {
-    if (currentUserIndex == -1) {
+void view_post()
+{
+    if (user_index == -1)
+    {
         printf("Please log in first!\n");
         return;
     }
 
-    printf("News Feed for %s:\n", socialNetwork[currentUserIndex].user.username);
-    for (int i = postCount - 1; i >= 0; i--) {
-        if (posts[i].authorId == currentUserIndex) {
+    printf("News Feed for %s:\n", social_network[user_index].user.username);
+    for (int i = post_count - 1; i >= 0; i--)
+    {
+        if (posts[i].authorId == user_index)
+        {
             printf("You added-- %s\n", posts[i].content);
-        } else {
-            printf("%s -- %s\n", socialNetwork[posts[i].authorId].user.username, posts[i].content);
+        }
+        else
+        {
+            printf("%s -- %s\n", social_network[posts[i].authorId].user.username, posts[i].content);
         }
     }
 }
 
-int main() {
+int main()
+{
     int choice;
 
-    while (1) {
+    while (1)
+    {
         printf("===========================================\n");
         printf("\tSocial Media Networking\n");
         printf("===========================================\n");
@@ -195,29 +234,30 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                registerUser();
-                break;
-            case 2:
-                login();
-                break;
-            case 3:
-                addFriend();
-                break;
-            case 4:
-                postUpdate();
-                break;
-            case 5:
-                viewPost();
-                break;
-            case 6:
-                viewFriends();
-                break;
-            case 7:
-                return 0;
-            default:
-                printf("Invalid choice. Try again.\n");
+        switch (choice)
+        {
+        case 1:
+            register_user();
+            break;
+        case 2:
+            login();
+            break;
+        case 3:
+            add_friend();
+            break;
+        case 4:
+            add_post();
+            break;
+        case 5:
+            view_post();
+            break;
+        case 6:
+            view_friends();
+            break;
+        case 7:
+            return 0;
+        default:
+            printf("Invalid choice. Try again.\n");
         }
     }
     return 0;
